4665b3ee83e8ef375b5c5a50d38fff9e4d57619d
joblackplay <jobson.loiola@gmail.com>
2026-04-09 19:39:17 -0300

    adcionando o alert

--- accounts/views.py
+++ accounts/views.py
@@ -9,17 +9,16 @@ def register(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            #form.save()
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
             username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
+            password = form.cleaned_data['password']
 
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,username = username, password = password)
-            login(request, user)
-            redirect('accounts/register.html')
+            return redirect('register')
     else:   
         form = SignUpForm()
         return render(request, 'accounts/register.html',{'form': form})
     
-    return render(request, 'register.html',{'form': form})
+    return render(request, 'accounts/register.html',{'form': form})
--- main/views.py
+++ main/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request,'home.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Login feito com Sucesso!")
+            return redirect('home')
+        else:
+            messages.success(request,"Ocorreu um erro ao efetuar o login. Por favor, tente novamente.")
+            return redirect('home')
+    else:    
+        return render(request,'home.html')
+    
+def logout_user(request):
+    logout(request)
