160663531a2b2f114bfb1c83647960a07bfaf444
joblackplay <jobson.loiola@gmail.com>
2026-04-28 15:40:59 -0300

    correçao de erros

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l^2&x6-6@u*6d+#g*ou_l%$iagk0*2ye9=l)$2bcugc7!2rj@o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,3 +127,4 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
+
